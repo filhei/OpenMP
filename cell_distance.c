dc1c4b71a17e7c7da35515ab38a72d08872f6458
Jens Nilsson <jenszza@gmail.com>
2018-10-13 17:19:25 +0200

    Added parallelization for parsing and computation

--- cell_distance.c
+++ cell_distance.c
@@ -1,3 +1,4 @@
+#include <omp.h>
 #include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -6,24 +7,14 @@
 #define BUFFER_SIZE 1e5
 #define MAX_DIST 34.64
 
+//global variables
+size_t file_position = 0;
+
 typedef struct{
 	short x, y, z;
 }Point;
 
-//set string value to one coord for a point
-static inline short atop(char *str){
-	short d;
-	d = 1e4*(str[1] - '0');
-	d += 1e3*(str[2] - '0');
-	d += 1e2*(str[4] - '0');
-	d += 1e1*(str[5] - '0');
-	d += str[6] - '0';
-	if(str[0]=='+'){
-		return d;
-	}
-	return -d;
-}
-
+//converts a row from the input file to a Point
 static inline void str2point(char *str, Point *p){
 	short d1,d2,d3;
 	d1 = 1e4*(str[1] - '0');
@@ -49,7 +40,7 @@ static inline void str2point(char *str, Point *p){
 	p->z = (str[16] == '+' ? d3 : -d3);
 }
 
-//might have to avround
+//converts float to string for output
 static inline void f2str(char *str, float f){
 	int temp = f*100;
 	str[0] = temp/1000 + '0';
@@ -85,43 +76,62 @@ static inline size_t i2str(char *str, int i){
 	}
 	return 0;
 }
-	
 
+/* WITH CURRENT IMPLEMENTATION DO NOT MULTITHREAD
+ * fread is performed by single thread, parsing multi*/	
 //input_file: the file to be read from, buffer: point buffer to hold the result
 static inline void read_file(FILE *input_file, Point *buffer){
-	static char result[9];
 	char *file_content = (char*) malloc(24*BUFFER_SIZE);
-	size_t bytes_read = fread(file_content,1,24*BUFFER_SIZE, input_file);
+	size_t bytes_read = fread(file_content,1,24*BUFFER_SIZE, input_file), current_byte = 0;
+
+	#pragma omp parallel for shared(buffer, file_content)
 	for(size_t current_byte = 0; current_byte < bytes_read; current_byte+=24){
 		str2point(&file_content[current_byte], &buffer[current_byte/24]);
 	}
 	free(file_content);
+	if(bytes_read != 24*BUFFER_SIZE){
+		//set file position to BOF
+		fseek(input_file,0,SEEK_SET);
+	}
 }
 
-static inline float point_dist(Point p1, Point p2){
-	return sqrt((float)
+static inline short point_index(Point p1, Point p2){
+	return (short)sqrt((float)
 			(p1.x-p2.x)*(p1.x-p2.x) +
 			(p1.y-p2.y)*(p1.y-p2.y) +
 			(p1.z-p2.z)*(p1.z-p2.z)
-		)/1000.0;
+		)/10.0 + 0.5;
 }
 
 int main(){
+	omp_set_num_threads(20);
 	FILE *fp;
-	fp = fopen("input_files/cell_e1","r");
+	fp = fopen("input_files/cell_e5","r");
 	Point *buffer = (Point*) malloc(BUFFER_SIZE*sizeof(Point));
 	read_file(fp, buffer);
 	//this needs to be thread safe
 	unsigned int output_occurance[3465];
-	for(int i = 0; i < 3465; ++i)
-		output_occurance[i] = 0;
+	memset(output_occurance,0,sizeof(int)*3465);
 
 	//+0.5 seems to fix the rounding errors
-	for(int i = 0; i < 1e1; ++i){
-		for(int j = 1e1-1; j > i; --j){
-			output_occurance[
-				(short)(point_dist(buffer[i], buffer[j])*100+0.5)
+	/*
+ 	* This part is where 99% of the time is spent.
+ 	* Making two copies and calculate both in the same for loop
+ 	* seemed to slightly increase performance, might be worth it
+ 	*/
+	#pragma omp parallel for
+	for(int i = 0; i < 100000; ++i){
+		int output_occurance_local[3465];
+		memset(output_occurance_local,0,sizeof(int)*3465);
+		for(int j = 100000-1; j > i; --j){
+			//awful locality, the index is random
+			output_occurance_local[
+				point_index(buffer[i], buffer[j])
 			] += 1;
+		}
+		#pragma omp critical
+		for(int j = 0; j < 3465; ++j){
+			output_occurance[j] += output_occurance_local[j];
 		}
 	}
 	char out_string[20*3465];
--- makefile
+++ makefile
@@ -1,17 +1,17 @@
 CC=gcc
-STD=-std=c11
+STD=-std=c99
 OFLAGS=-O2 -ffast-math
-LIBFLAGS=-lm
+LIBFLAGS=-lm -lgomp
 
 .PHONY: all clean
 
 all: cell_distance cell_debug
 
 cell_distance:cell_distance.c
-	$(CC) $(OFLAGS) $(STD) -o $@ $< $(LIBFLAGS)
+	$(CC) -fopenmp $(OFLAGS) $(STD) -o $@ $< $(LIBFLAGS)
 
-cell_debug:cell_distance.c
-	$(CC) $(STD) -g -o $@ $< $(LIBFLAGS)
+#cell_debug:cell_distance.c
+#	$(CC) $(STD) -g -o $@ $< $(LIBFLAGS)
 
 clean:
 	rm cell_distance
