0c826a38af220507c5a490b1e7c29673ae309969
Jens Nilsson <jenszza@gmail.com>
2018-10-13 19:03:25 +0200

    Counting number of points

--- cell_distance.c
+++ cell_distance.c
@@ -1,21 +1,38 @@
+/* TODO
+ * Must refill buffer from time to time
+ * Make file reading thread safe
+ * Optimize calculations
+*/
+
 #include <omp.h>
 #include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
 
-#define BUFFER_SIZE 1e5
+#define POINTS_PER_BUFFER 1e5
 #define MAX_DIST 34.64
-
-//global variables
-size_t file_position = 0;
+#define OUT_BUFFER_SIZE 3465
+#define MAX_NUM_OF_POINTS 2147483648
 
 typedef struct{
 	short x, y, z;
 }Point;
 
+//this might not be necessary
+size_t probe_num_of_points(FILE *input_file){
+	char *buffer = (char*)malloc(24*POINTS_PER_BUFFER);
+	size_t num_of_points = 0, points_read = POINTS_PER_BUFFER;
+	for(points_read; points_read == POINTS_PER_BUFFER; num_of_points+=points_read){
+		points_read = fread(buffer, 24, POINTS_PER_BUFFER, input_file);
+	}
+	free(buffer);
+	fseek(input_file, 0, SEEK_SET);
+	return num_of_points;
+}
+
 //converts a row from the input file to a Point
-static inline void str2point(char *str, Point *p){
+static inline void str2point(char * restrict str, Point * restrict p){
 	short d1,d2,d3;
 	d1 = 1e4*(str[1] - '0');
 	d1 += 1e3*(str[2] - '0');
@@ -80,16 +97,16 @@ static inline size_t i2str(char *str, int i){
 /* WITH CURRENT IMPLEMENTATION DO NOT MULTITHREAD
  * fread is performed by single thread, parsing multi*/	
 //input_file: the file to be read from, buffer: point buffer to hold the result
-static inline void read_file(FILE *input_file, Point *buffer){
-	char *file_content = (char*) malloc(24*BUFFER_SIZE);
-	size_t bytes_read = fread(file_content,1,24*BUFFER_SIZE, input_file), current_byte = 0;
+static inline void read_file(FILE * restrict input_file, Point * restrict buffer){
+	char *file_content = (char*) malloc(24*POINTS_PER_BUFFER);
+	size_t bytes_read = fread(file_content,1,24*POINTS_PER_BUFFER, input_file), current_byte = 0;
 
 	#pragma omp parallel for shared(buffer, file_content)
 	for(size_t current_byte = 0; current_byte < bytes_read; current_byte+=24){
 		str2point(&file_content[current_byte], &buffer[current_byte/24]);
 	}
 	free(file_content);
-	if(bytes_read != 24*BUFFER_SIZE){
+	if(bytes_read != 24*POINTS_PER_BUFFER){
 		//set file position to BOF
 		fseek(input_file,0,SEEK_SET);
 	}
@@ -107,40 +124,34 @@ int main(){
 	omp_set_num_threads(20);
 	FILE *fp;
 	fp = fopen("input_files/cell_e5","r");
-	Point *buffer = (Point*) malloc(BUFFER_SIZE*sizeof(Point));
+	size_t number_of_points = probe_num_of_points(fp);
+	Point *buffer = (Point*) malloc(POINTS_PER_BUFFER*sizeof(Point));
 	read_file(fp, buffer);
 	//this needs to be thread safe
-	unsigned int output_occurance[3465];
-	memset(output_occurance,0,sizeof(int)*3465);
-
+	unsigned int output_occurance[OUT_BUFFER_SIZE];
+	memset(output_occurance,0,sizeof(int)*OUT_BUFFER_SIZE);
 	//+0.5 seems to fix the rounding errors
 	/*
  	* This part is where 99% of the time is spent.
  	* Making two copies and calculate both in the same for loop
  	* seemed to slightly increase performance, might be worth it
  	*/
-	#pragma omp parallel for
-	for(int i = 0; i < 100000; ++i){
-		int output_occurance_local[3465];
-		memset(output_occurance_local,0,sizeof(int)*3465);
-		for(int j = 100000-1; j > i; --j){
+/*	#pragma omp parallel for reduction(+:output_occurance[:OUT_BUFFER_SIZE])
+	for(int i = 0; i < number_of_points; ++i){
+		for(int j = number_of_points-1; j > i; --j){
 			//awful locality, the index is random
-			output_occurance_local[
+			++output_occurance[
 				point_index(buffer[i], buffer[j])
-			] += 1;
-		}
-		#pragma omp critical
-		for(int j = 0; j < 3465; ++j){
-			output_occurance[j] += output_occurance_local[j];
+			];
 		}
 	}
-	char out_string[20*3465];
+	char out_string[20*OUT_BUFFER_SIZE];
 	char temp[20];
 	int size = 0, i2str_count = 0;
 	//might be a good idea to combine i2str and f2str for performance
 	for(int i = 0; i < 3465; ++i){
 		if(output_occurance[i]){
-			f2str(temp, MAX_DIST*i/3464.0);
+			f2str(temp, MAX_DIST*i/((float) OUT_BUFFER_SIZE-1));
 			temp[5] = ' ';
 			i2str_count = i2str(&temp[6], output_occurance[i]);
 			memcpy(&out_string[size], temp, 6+i2str_count);
@@ -148,7 +159,7 @@ int main(){
 		}
 	}
 	printf(out_string);
-	
+*/	
 	fclose(fp);
 	free(buffer);
 	return 0;
