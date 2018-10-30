9187ac686c3722e89aeecd24d8115335abfe9cc3
Jens Nilsson <jenszza@gmail.com>
2018-10-30 12:17:03 +0100

    fixed for submission

--- cell_distance.c
+++ cell_distance.c
@@ -1,25 +1,22 @@
-/* TODO
- * Must refill buffer from time to time
- * Make file reading thread safe
- * Optimize calculations
-*/
-
 #include <omp.h>
 #include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
 
-#define POINTS_PER_BUFFER 10000
+#define POINTS_PER_BUFFER 100000
 #define OUT_BUFFER_SIZE 3465
 #define MAX_NUM_OF_POINTS 2147483648
 
+size_t _num_of_threads;
+
+/* defines a point in 3-dimensional space */
 typedef struct{
 	short x, y, z;
 }Point;
 
-//converts a row from the input file to a Point
-static inline void str2point(char * restrict const str, Point * restrict p){
+/* converts a row from the input file to a Point */
+static inline void str2point(char const str[], Point * restrict p){
 	short d1,d2,d3;
 	d1 = 1e4*(str[1] - '0');
 	d1 += 1e3*(str[2] - '0');
@@ -44,8 +41,8 @@ static inline void str2point(char * restrict const str, Point * restrict p){
 	p->z = (str[16] == '+' ? d3 : -d3);
 }
 
-//converts float to string for output
-static inline void index2str(char *str, short s){
+/* converts float to string for output */
+static inline void index2str(char str[], short s){
 	str[0] = s/1000 + '0';
 	s = s%1000;
 	str[1] = s/100 + '0';
@@ -55,7 +52,8 @@ static inline void index2str(char *str, short s){
 	str[4] = s%10 + '0';
 }
 
-static inline size_t i2str(char *str, int i){
+/* converts an int to string for output */
+static inline size_t i2str(char str[], int i){
 	if(i < 10){
 		str[0] = i + '0';
 		str[1] = '\n';
@@ -80,6 +78,7 @@ static inline size_t i2str(char *str, int i){
 	return 0;
 }
 
+/* read and parse input file */
 const int read_points(
 		FILE * restrict input_file,
 		Point start_points[],
@@ -89,25 +88,24 @@ const int read_points(
 ){
 	size_t static bytes_read,  position_in_file = 0;
 	char static file_content[24*POINTS_PER_BUFFER];
-	//first check if we need to read to start_points
+	/* first check if we need to read to start_points */
 	if(position_in_file == 0 || feof(input_file)){
 		fseek(input_file, position_in_file, SEEK_SET);
 		bytes_read = fread(file_content, 1, 24*POINTS_PER_BUFFER, input_file);
 		position_in_file += bytes_read;
-		//read same content to start and end since this is a new triangular block
+		/* read same content to start and end since this is a new triangular block */
 		#pragma omp parallel for
 		for(size_t current_byte = 0; current_byte < bytes_read; current_byte+=24){
 			str2point(&file_content[current_byte], &start_points[current_byte/24]);
-			str2point(&file_content[current_byte], &end_points[current_byte/24]);
 		}
 		memcpy(end_points, start_points, 3*sizeof(short)*bytes_read/24);
 		*start_buffer_size = bytes_read/24;
 		*end_buffer_size = bytes_read/24;
-		// return value should reflect if block or triangular or if done
+		/* return value should reflect if block or triangular or if done */
 		return bytes_read == 0 ? 2:1;
 	}
 	bytes_read = fread(file_content, 1, 24*POINTS_PER_BUFFER, input_file);
-	#pragma omp parallel for
+	#pragma omp parallel for 
 	for(size_t current_byte = 0; current_byte < bytes_read; current_byte+=24){
 		str2point(&file_content[current_byte], &end_points[current_byte/24]);
 	}
@@ -115,14 +113,16 @@ const int read_points(
 	return 0;
 }
 
-static inline const short point_index(const Point p1, const Point p2){ //use intrinsics (SIMD)
-	return (short)(sqrtf(
+/* calculate distance from p1 to p2 and return the index in the occurance array */
+static inline const short point_index(const Point p1, const Point p2){
+	return (short)(sqrt(
 			(p1.x-p2.x)*(p1.x-p2.x) +
 			(p1.y-p2.y)*(p1.y-p2.y) +
 			(p1.z-p2.z)*(p1.z-p2.z)
 		)/10.0 + 0.5);
 }
 
+/* count occurance for a rectangular block */
 static inline void calc_block(
 		const Point start_points[],
 		const Point end_points[],
@@ -130,13 +130,16 @@ static inline void calc_block(
 		const size_t end_length,
 		unsigned int output[]
 ){
-	#pragma omp parallel for collapse(2) reduction(+:output[:OUT_BUFFER_SIZE])
+	const size_t chunk_size = (start_length/(10*_num_of_threads) == 0 ? 1 : (start_length/(10*_num_of_threads)));
+	/* increment output vector for distances between every point in "block" in parallell */
+	#pragma omp parallel for schedule(static,chunk_size) collapse(2) reduction(+:output[:OUT_BUFFER_SIZE])
 	for(int i = 0; i < start_length; i+=2) {
 		for(int j = 0; j < end_length; ++j) {
 			++output[point_index(start_points[i], end_points[j])];
 			++output[point_index(start_points[i+1], end_points[j])];
 		}
 	}
+	/* if number of points are uneven, increment output for final distances */
 	if(start_length%2){
 		for(size_t j = 0; j < end_length; ++j){
 			++output[point_index(start_points[start_length-1], end_points[j])];
@@ -144,21 +147,22 @@ static inline void calc_block(
 	}
 }
 
+/* count occurance for a triangular block */
 static inline void calc_triangle(
 		const Point start_points[],
 		const Point end_points[],
 		const size_t length,
 		unsigned int output[]
 ){
-	#pragma omp parallel for reduction(+:output[:OUT_BUFFER_SIZE])
+	const size_t chunk_size = (length/(10*_num_of_threads) == 0 ? 1 : (length/(10*_num_of_threads)));
+	/* increment output vector for distances between every point in "block" in parallell */
+	#pragma omp parallel for schedule(dynamic,chunk_size) reduction(+:output[:OUT_BUFFER_SIZE])
 	for(size_t i = 0; i < length; ++i) {
-		for(size_t j = i+1; j < length-length%2; j+=2) {
+		for(size_t j = i+1; j < length; ++j) {
 			++output[point_index(start_points[i], end_points[j])];
-			++output[point_index(start_points[i], end_points[j+1])];
 		}
 	}
 }
-
 
 int main(int argc, char *argv[]){
 	if(argc != 2){
@@ -167,18 +171,21 @@ int main(int argc, char *argv[]){
 	if(argv[1][0] != '-' || argv[1][1] != 't'){
 		exit(-2);
 	}
-	size_t num_of_threads = strtol(&argv[1][2], NULL, 10);
-	omp_set_num_threads(num_of_threads);
+	_num_of_threads = strtol(&argv[1][2], NULL, 10);
+	omp_set_num_threads(_num_of_threads);
 	FILE *fp;
-//	fp = fopen("input_files/cell_e5","r");
 	fp = fopen("cells","r");
+	if(fp == NULL){
+		printf("error opening file\n");
+		exit(1);
+	}
 	Point *start_points = (Point*) malloc(2*POINTS_PER_BUFFER*sizeof(Point));
 	Point *end_points = start_points+POINTS_PER_BUFFER;
 	unsigned int output_occurance[OUT_BUFFER_SIZE];
 	memset(output_occurance,0,sizeof(int)*OUT_BUFFER_SIZE);
 	size_t start_length, end_length;
 	int return_value;
-	do{ //TODO: check the return values, that they are correct
+	do{
 		return_value = read_points(fp, start_points, end_points, &start_length, &end_length);
 		if(return_value == 0){
 			calc_block(start_points, end_points, start_length, end_length, output_occurance);
@@ -186,7 +193,9 @@ int main(int argc, char *argv[]){
 			calc_triangle(start_points, end_points, start_length, output_occurance);
 		}
 	}while(return_value != 2);
-	char out_string[20*OUT_BUFFER_SIZE]; //hold the whole output_string
+
+	/* form the output */
+	char out_string[20*OUT_BUFFER_SIZE];
 	char temp[20];
 	int size = 0, i2str_count = 0;
 	for(int i = 0; i < OUT_BUFFER_SIZE; ++i){
--- makefile
+++ makefile
@@ -3,16 +3,12 @@ STD=-std=c11
 OFLAGS=-O3 -ffast-math
 LIBFLAGS=-lm -lgomp
 
-.PHONY: all clean
+.PHONY: all clean bench
 
-all: cell_distance debug_cell
+all: cell_distance
 
 cell_distance:cell_distance.c
 	$(CC) -fopenmp $(OFLAGS) $(STD) -o $@ $< $(LIBFLAGS)
 
-debug_cell: cell_distance.c
-	$(CC) -openmp $(STD) -g -o $@ $< $(LIBFLAGS)
-
 clean:
 	rm cell_distance
-	rm debug_cell
